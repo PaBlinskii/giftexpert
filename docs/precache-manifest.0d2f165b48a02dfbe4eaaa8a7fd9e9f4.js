58116df04bdd67f1ef116361ab5c9b07a9cd49d1
pablinskii <ipxb10@gmail.com>
2021-11-09 23:13:17 -0600

    add docs

---
+++ docs/precache-manifest.0d2f165b48a02dfbe4eaaa8a7fd9e9f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b22c4e18b226c160ab1f819b972cb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01385303796fbc4eb633",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01385303796fbc4eb633",
+    "url": "/static/js/main.993abd45.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
